1d1f8429f8681621ffb3b05b4ea22dbae85113fc
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-16 14:36:47 +0530

    feat(retail-inventory): implement product selection and purchase quantity input logic

--- retailInventoryBilling.py
+++ retailInventoryBilling.py
@@ -1,4 +1,4 @@
-# UC5 – Display Available Products in Inventory
+# UC6 – Select Product and Enter Purchase Quantity
 
 inventory = {
     "apple": {"price": 30, "quantity": 100},
@@ -6,10 +6,19 @@ inventory = {
     "milk": {"price": 50, "quantity": 40}
 }
 
-print("Available Products in Inventory:\n")
-
+print("Available Products:")
 for product, details in inventory.items():
-    print(f"Product: {product}")
-    print(f"Price: {details['price']}")
-    print(f"Quantity: {details['quantity']}")
-    print("-----------------------")
+    print(product, "Price:", details["price"], "Quantity:", details["quantity"])
+
+product_name = input("\nEnter product name to purchase: ").lower()
+
+if product_name in inventory:
+    purchase_qty = int(input("Enter quantity to purchase: "))
+
+    if purchase_qty <= inventory[product_name]["quantity"]:
+        print("Product selected:", product_name)
+        print("Quantity to purchase:", purchase_qty)
+    else:
+        print("Insufficient stock available.")
+else:
+    print("Product not found in inventory.")
