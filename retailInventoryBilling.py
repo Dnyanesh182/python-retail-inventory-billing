19ac1a3da4c3e4726e257f4568771a64f1732628
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-16 14:26:48 +0530

    feat(retail-inventory): implement functionality to update product quantity in inventory

--- retailInventoryBilling.py
+++ retailInventoryBilling.py
@@ -1,19 +1,19 @@
-# UC2 – Add New Product to Inventory
+# UC3 – Update Product Quantity in Inventory
 
 inventory = {
     "apple": {"price": 30, "quantity": 100},
-    "banana": {"price": 10, "quantity": 150}
+    "banana": {"price": 10, "quantity": 150},
+    "milk": {"price": 50, "quantity": 40}
 }
 
-product_name = input("Enter product name: ").lower()
-price = int(input("Enter product price: "))
-quantity = int(input("Enter product quantity: "))
+product_name = input("Enter product name to update quantity: ").lower()
 
 if product_name in inventory:
-    print("Product already exists in inventory.")
+    new_quantity = int(input("Enter new quantity: "))
+    inventory[product_name]["quantity"] = new_quantity
+    print("Product quantity updated successfully.")
 else:
-    inventory[product_name] = {"price": price, "quantity": quantity}
-    print("Product added successfully.")
+    print("Product not found in inventory.")
 
 print("\nUpdated Inventory:")
 for product, details in inventory.items():
